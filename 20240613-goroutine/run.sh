47b3f05f744b9a3aeb1f4bc9757aef85f32601f6
Muhammad Iqbal Alaydrus <iqbal@bangunindo.com>
2024-06-21 20:24:01 +0700

    fix build

--- 20240613-goroutine/run.sh
+++ 20240613-goroutine/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 rm -f output.csv plot-golang.png
+mkdir -p dist
 go mod download
 go build -o dist/goroutine 20240613-goroutine
 psrecord './dist/goroutine' --include-children --plot plot-golang.png
