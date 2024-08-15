fc91493909b4eaf5a9f0767ce783dc2e93611462
Muhammad Iqbal Alaydrus <m.iqbal.alaydrus@gmail.com>
2024-08-15 17:18:24 +0700

    mmap for faster IO

--- 20240809-cpp-data/main.cpp
+++ 20240809-cpp-data/main.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <fstream>
 #include <unordered_map>
+#include <boost/iostreams/device/mapped_file.hpp>
+#include <boost/iostreams/stream.hpp>
 
 struct locationData {
     uint64_t count = 0;
@@ -12,13 +14,50 @@ struct locationData {
     float_t temp_max = -99;
 };
 
-void line_process() {
+namespace io = boost::iostreams;
 
+int main_mmap() {
+    using namespace boost::iostreams;
+    auto start = std::chrono::system_clock::now();
+    char data[0x1000];
+
+    io::stream_buffer<io::mapped_file_source> file("measurements.txt");
+    std::istream in(&file);
+    while (in)
+    {
+        in.read(data, 0x1000);
+        // do something with data
+    }
+    file.close();
+
+    auto end = std::chrono::system_clock::now();
+    typedef std::chrono::duration<float> fsec;
+    fsec fs = end - start;
+    std::cout << fs.count() << "s\n";
+    return 0;
 }
 
-int main() {
+int main_ifstream() {
     auto start = std::chrono::system_clock::now();
-    std::ifstream file("measurements-small.txt", std::ios_base::in);
+    char data[0x1000];
+    std::ifstream in("measurements.txt");
+
+    while (in)
+    {
+        in.read(data, 0x1000);
+        // do something with data
+    }
+    in.close();
+    auto end = std::chrono::system_clock::now();
+    typedef std::chrono::duration<float> fsec;
+    fsec fs = end - start;
+    std::cout << fs.count() << "s\n";
+    return 0;
+}
+
+int main_old() {
+    auto start = std::chrono::system_clock::now();
+    std::ifstream file{"measurements.txt", std::ios_base::in};
     if (!file) std::cerr << "Can't open input file!";
     if (file.is_open()) {
         std::string line;
@@ -30,7 +69,7 @@ int main() {
             uint64_t pos = line.find(delimiter);
             std::string location = line.substr(0, pos);
             std::string temperature_str = line.substr(pos+1, line.length());
-            float temperature_num = std::stof(temperature_str);
+            auto temperature_num = std::stof(temperature_str);
             if (!loc_data.contains(location)) {
                 loc_data[location] = locationData{
                     .count = 1,
@@ -58,4 +97,8 @@ int main() {
     fsec fs = end - start;
     std::cout << fs.count() << "s\n";
     return 0;
+}
+
+int main() {
+    return main_mmap();
 }
