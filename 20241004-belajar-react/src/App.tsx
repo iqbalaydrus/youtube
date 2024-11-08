d8a4bb3de8e05c43ecf86bacb7431d53e8aa9350
Muhammad Iqbal Alaydrus <iqbal@bangunindo.com>
2024-11-08 18:40:00 +0700

    react

---
+++ 20241004-belajar-react/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+        <div className={"min-h-screen w-screen flex items-center justify-center"}>
+            <div className={"mx-center py-3 rounded-md border-2"}>
+                <div className={"flex flex-row items-center justify-between"}>
+                    <div className={"col-start-3 text-2xl font-semibold text-white"}>Masuk</div>
+                    <div className={"col-end-3 text-white"}>Masuk</div>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default App
